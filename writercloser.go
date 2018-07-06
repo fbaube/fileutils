02d94fa8e2c92ca77e7a200417bc48ec6b968498
F.Baube <fbaube@welho.com>
2018-07-06 19:04:10 +0300

    Deleted cos was a dupe of a stdlib function

--- writercloser.go
+++
@@ -1,9 +0,0 @@
-package fileutils
-
-// WriterCloser is like an io.Discard but it implements io.CLoser too.
-type WriterCloser struct {
-}
-
-func (*WriterCloser) Write(p []byte) (int, error) { return 0, nil }
-
-func (*WriterCloser) Close() error { return nil }
